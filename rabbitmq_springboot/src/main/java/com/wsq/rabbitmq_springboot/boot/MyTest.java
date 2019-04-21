178899c6ad2d147f378c793ae48957c5ec2c140e
shenqin.wang <shenqin0711@163.com>
2019-04-21 16:02:58 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	rabbitmq_springboot/src/main/java/com/wsq/rabbitmq_springboot/boot/MyTest.java

--- rabbitmq_springboot/src/main/java/com/wsq/rabbitmq_springboot/boot/MyTest.java
+++ rabbitmq_springboot/src/main/java/com/wsq/rabbitmq_springboot/boot/MyTest.java
@@ -3,6 +3,7 @@ package com.wsq.rabbitmq_springboot.boot;
 public class MyTest {
     public static void main(String[] args){
         System.out.println("sjadjhsajdhshdabdbh");
-        System.out.println("我是客户端，我也要输出111！");
+
+        System.out.println("我是客户端，我也要输出！");
     }
 }
