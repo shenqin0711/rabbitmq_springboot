3e37484f9123c8663094bf9fd2909ecfc7d04ff9
shenqin.wang <shenqin0711@163.com>
2019-04-21 12:09:30 +0800

    添加了输出

--- rabbitmq_springboot/src/main/java/com/wsq/rabbitmq_springboot/boot/MyTest.java
+++ rabbitmq_springboot/src/main/java/com/wsq/rabbitmq_springboot/boot/MyTest.java
@@ -3,5 +3,6 @@ package com.wsq.rabbitmq_springboot.boot;
 public class MyTest {
     public static void main(String[] args){
         System.out.println("sjadjhsajdhshdabdbh");
+		System.out.println("sjadjhsajdhshdabdbh");
     }
 }
